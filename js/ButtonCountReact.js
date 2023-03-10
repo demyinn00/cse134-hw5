06fb94ae6397985e60cfa504d922aa07a0245829
David Em-Yinn <demyinn@ucsd.edu>
2023-03-10 03:53:27 -0800

    started ec, but having trouble

---
+++ js/ButtonCountReact.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+function ButtonCount() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <button onClick={() => setCount(count + 1)}>
+      {count}
+    </button>
+  );
+}
+
+export default ButtonCount;
